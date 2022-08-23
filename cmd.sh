a01cb1f04cf0af909e4aad5143544d99b49832b3
younguru <kiosun@mail.ru>
2022-08-23 16:56:35 +0300

    kind start

--- cmd.sh
+++ cmd.sh
@@ -1,7 +1,8 @@
+docker compose -f docker-compose.yml up -d
 ./gradlew clean build
 docker build . -t soldat2d/k8s-test
 docker run -ti --rm soldat2d/k8s-test
 docker rmi soldat2d/k8s-test
-docker run -ti --rm -e DATASOURCE_HOST=192.168.1.158 -p 8081:8080 soldat2d/k8s-test
+docker run -ti --rm -e DATASOURCE_HOST=192.168.8.212 -p 8081:8080 soldat2d/k8s-test
 docker login
 docker push soldat2d/k8s-test
