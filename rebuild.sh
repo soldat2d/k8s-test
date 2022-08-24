8279840ba65a705c70a5c58ffe5f2c8ede6e7eac
younguru <kiosun@mail.ru>
2022-08-24 16:53:20 +0300

    503 error finish

---
+++ rebuild.sh
@@ -0,0 +1,6 @@
+kubectl delete deployments k8s-test-deployment
+kubectl delete service k8s-service
+kubectl delete ingress k8s-ingress
+kubectl apply -f k8s/deployment.yml
+kubectl apply -f k8s/service.yml
+kubectl apply -f k8s/ingress.yml
