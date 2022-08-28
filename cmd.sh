85eae091df4f63d309f1a1e4ba8d1f5e908d93fe
soldat2d <ten.soldat@gmail.com>
2022-08-29 00:53:17 +0300

    Finish

--- cmd.sh
+++ cmd.sh
@@ -1,8 +1,9 @@
+kind create cluster --config kind-config.yml
 docker compose -f docker-compose.yml up -d
 ./gradlew clean build
 docker build . -t soldat2d/k8s-test
 docker run -ti --rm soldat2d/k8s-test
 docker rmi soldat2d/k8s-test
-docker run -ti --rm -e DATASOURCE_HOST=192.168.8.212 -p 8081:8080 soldat2d/k8s-test
 docker login
 docker push soldat2d/k8s-test
+docker run -ti --rm -e DATASOURCE_HOST=192.168.8.212 -p 8081:8080 soldat2d/k8s-test
--- rebuild.sh
+++ rebuild.sh
@@ -4,3 +4,6 @@ kubectl delete ingress k8s-ingress
 kubectl apply -f k8s/deployment.yml
 kubectl apply -f k8s/service.yml
 kubectl apply -f k8s/ingress.yml
+kubectl get pods
+kubectl get services
+kubectl get ingress
